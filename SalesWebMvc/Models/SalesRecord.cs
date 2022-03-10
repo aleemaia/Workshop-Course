19825b95d6e22aa87630f958b3e3d75bf65e4b52
Alexandre Maia <alexandre--maia@hotmail.com>
2022-03-10 20:08:36 -0300

    Seeding Service

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -7,13 +7,13 @@ namespace SalesWebMvc.Models {
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
-        public SalesStatus Status { get; set; }
+        public SaleStatus Status { get; set; }
         public Seller Saller { get; set; }
 
         public SalesRecord() {
         }
 
-        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller saller) {
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller saller) {
             Id = id;
             Date = date;
             Amount = amount;
